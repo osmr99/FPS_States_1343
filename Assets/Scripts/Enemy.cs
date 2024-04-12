6d1e4d57cf9ab141a5ca5cb934f3c7f7349fb485
osmr99 <omarsantiagomorales@gmail.com>
2024-04-12 14:38:42 -0500

    working on it

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,9 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
+
+public enum MovingObjectStates
+{
+    wander,
+    pursue,
+    attack,
+    recovery
+}
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] MovingObjectStates currentState;
+    Vector3 startingLocation;
+    [SerializeField] float wanderRange;
+    [SerializeField] float playerSightRange;
+    [SerializeField] float playerAttackRange;
+    [SerializeField] float currentStateElapsed;
+    [SerializeField] float recoveryTime;
+
+
     public Rigidbody Rigidbody { get; private set; }
     Vector3 origin;
     
@@ -12,12 +30,26 @@ public class Enemy : MonoBehaviour
     {
         Rigidbody = GetComponent<Rigidbody>();
         origin = transform.position;
+        startingLocation = origin;
     }
 
     // Update is called once per frame
     void Update()
     {
+        switch(currentState)
+        {
+            case MovingObjectStates.wander:
+                break;
 
+            case MovingObjectStates.pursue:
+                break;
+
+            case MovingObjectStates.attack:
+                break;
+
+            case MovingObjectStates.recovery:
+                break;
+        }
     }
 
     public void ApplyKnockback(Vector3 knockback)
